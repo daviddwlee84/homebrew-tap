2345a8e0427da1c1138f833ef69ccb974be7be20
David Lee <daviddwlee84@gmail.com>
2026-09-17 09:51:24 +0000

    dev-cli 0.2.39

--- Formula/dev-cli.rb
+++ Formula/dev-cli.rb
@@ -1,8 +1,8 @@
 class DevCli < Formula
   desc "Task and worktree command center for multi-repository development"
   homepage "https://github.com/daviddwlee84/dev-cli"
-  url "https://github.com/daviddwlee84/dev-cli/archive/refs/tags/v0.2.38.tar.gz"
-  sha256 "03e15c3fb7f6f1104f7a26736e545bd457e47e2032326e7084f991bf04c6dd28"
+  url "https://github.com/daviddwlee84/dev-cli/archive/refs/tags/v0.2.39.tar.gz"
+  sha256 "f58ea0a4d8dbbda0b07bc8367bd1d891c7e14ee98bcf96727686355dc9fa4ecd"
   license "MIT"
   head "https://github.com/daviddwlee84/dev-cli.git", branch: "main"
 
